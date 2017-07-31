022acb8c46c55e52586e717c740888665680704e
Adam Biessener <abiessener@gmail.com>
2017-07-31 13:20:00 -0500

    added basic div generation to button

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('script sourced');
+
+
+
+$(document).ready(function(){
+    $('#genButton').on('click', function(){
+        $(this).parent().append('<div class=\'addedDiv\'></div>');
+    });
+
+});
